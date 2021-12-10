8a771f437a713e0c7d6804918652870e446b1794
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-09 23:55:04 -0300

    refactor: init get route

--- src/routes.ts
+++ src/routes.ts
@@ -1,23 +1,8 @@
-import {UserController} from "./controller/UserController";
+import { Router } from "express"
+import UserController from "./controllers/UserController"
 
-export const Routes = [{
-    method: "get",
-    route: "/users",
-    controller: UserController,
-    action: "all"
-}, {
-    method: "get",
-    route: "/users/:id",
-    controller: UserController,
-    action: "one"
-}, {
-    method: "post",
-    route: "/users",
-    controller: UserController,
-    action: "save"
-}, {
-    method: "delete",
-    route: "/users/:id",
-    controller: UserController,
-    action: "remove"
-}];
+const routes = Router()
+
+routes.get('/', UserController.getUsers)
+
+export default routes
