5bfffb59d2a95079982730d3bcc1985210cb7360
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-10 00:37:37 -0300

    feat: add method to create a new user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -20,6 +20,23 @@ class UserController {
             })
         }
     }
+
+    async createUser(req: Request, res: Response<IResponse>): Promise<Response<IResponse>> {
+        try{
+            const userRepository = getRepository(User)
+            const created = await userRepository.save(req.body)
+            return res.json({
+                status: ResponseStatus.OK,
+                data: created
+            })
+        }catch(error){
+            return res.status(500).json({
+                status: ResponseStatus.INTERNAL_SERVER_ERROR,
+                message: 'An internal server error has happened.',
+                errors: error
+            })
+        }
+    }
 }
 
 export default new UserController()
