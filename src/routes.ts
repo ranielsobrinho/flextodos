dc5dc32c555f7ed271efc7df5cc59e5c4274558f
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-10 00:37:51 -0300

    feat: add route to create a new user

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import UserController from "./controllers/UserController"
 
 const routes = Router()
 
-routes.get('/', UserController.getUsers)
+routes.get('/users', UserController.getUsers)
+routes.post('/users', UserController.createUser)
 
 export default routes
