df407c3726befd1e832a32180d95e18c3e1cdd26
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-09 23:59:03 -0300

    feat: add interface to all response controllers methods

---
+++ src/utils/service.ts
@@ -0,0 +1,14 @@
+export enum ResponseStatus {
+  BAD_REQUEST = 'BAD_REQUEST',
+  NOT_FOUND = 'NOT_FOUND',
+  INTERNAL_SERVER_ERROR = 'INTERNAL_SERVER_ERROR',
+  OK = 'OK'
+}
+
+export interface IResponse {
+  status: ResponseStatus
+  data?: any[] | any
+  message?: string
+  errors?: string[]
+  totalRegisters?: number
+}
