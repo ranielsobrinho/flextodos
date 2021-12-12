c8843d7f74bfa87c1eab9c09ca6a536f24a4e910
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-12 10:40:09 -0300

    feat: add new routes to manage todo's data

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express"
+import TodoController from "./controllers/TodoController"
 import UserController from "./controllers/UserController"
 
 const routes = Router()
@@ -9,5 +10,10 @@ routes.post('/users', UserController.createUser)
 routes.put('/users/:id', UserController.updateUser)
 routes.delete('/users/:id', UserController.deleteUser)
 
+routes.get('/todos', TodoController.getTodo)
+routes.get('/todos/:id', TodoController.getOneTodo)
+routes.post('/todos', TodoController.createTodo)
+routes.put('/todos/:id', TodoController.updateTodo)
+routes.delete('/todos/:id', TodoController.deleteTodo)
 
 export default routes
