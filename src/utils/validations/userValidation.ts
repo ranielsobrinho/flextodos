dba28c21c6b417945309c293afe411f3e30ec587
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-12 16:31:07 -0300

    refactor: change password type to string

--- src/utils/validations/userValidation.ts
+++ src/utils/validations/userValidation.ts
@@ -4,7 +4,7 @@ const userSchema = yup.object({
   name: yup.string().min(5).max(100).required(),
   username: yup.string().min(5).max(50).required(),
   email: yup.string().email().required(),
-  password: yup.number().min(8).required()
+  password: yup.string().min(8).required()
 })
 
 export default userSchema
