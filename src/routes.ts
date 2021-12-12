8a6407d718d9f0878c16b0e7e9fa919893b27db3
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-12 16:31:36 -0300

    feat: add new auth route and middlewares to todos routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,22 @@
 import { Router } from "express"
-import TodoController from "./controllers/TodoController"
-import UserController from "./controllers/UserController"
+import { TodoController, UserController, AuthController } from "./controllers/"
+import { authMiddleware, validate } from "./middlewares"
+import { todoSchema, userSchema, authSchema } from "./utils/validations"
 
 const routes = Router()
 
 routes.get('/users', UserController.getUsers)
 routes.get('/users/:id', UserController.getOneUser)
-routes.post('/users', UserController.createUser)
-routes.put('/users/:id', UserController.updateUser)
-routes.delete('/users/:id', UserController.deleteUser)
+routes.post('/users', validate(userSchema),UserController.createUser)
+routes.put('/users/:id', authMiddleware.auth, validate(userSchema), UserController.updateUser)
+routes.delete('/users/:id', authMiddleware.auth, UserController.deleteUser)
 
-routes.get('/todos', TodoController.getTodo)
-routes.get('/todos/:id', TodoController.getOneTodo)
-routes.post('/todos', TodoController.createTodo)
-routes.put('/todos/:id', TodoController.updateTodo)
-routes.delete('/todos/:id', TodoController.deleteTodo)
+routes.get('/todos', authMiddleware.auth , TodoController.getTodo)
+routes.get('/todos/:id', authMiddleware.auth, TodoController.getOneTodo)
+routes.post('/todos', authMiddleware.auth, validate(todoSchema), TodoController.createTodo)
+routes.put('/todos/:id', authMiddleware.auth, validate(todoSchema), TodoController.updateTodo)
+routes.delete('/todos/:id', authMiddleware.auth, TodoController.deleteTodo)
+
+routes.post('/auth', validate(authSchema), AuthController.authenticate)
 
 export default routes
