761b38f7ed8efa449228dc5ec53eda1b0e250ec0
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-15 16:08:44 -0300

    refactor: new value for user id of schema

--- src/utils/validations/todoValidation.ts
+++ src/utils/validations/todoValidation.ts
@@ -2,7 +2,7 @@ import * as yup from 'yup'
 
 const todoSchema = yup.object({
   content: yup.string().min(5).required(),
-  userId: yup.number().min(1).max(1).required()
+  userId: yup.number().required()
 })
 
 export default todoSchema
